62c2483e76d9542466a9d9c3e2455e472613ca50
Damien Mellot <d.mellot@hotmail.fr>
2026-01-22 03:25:52 +0100

    fix

---
+++ app/components/shared/interface-conversation/components/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Export centralisé des composants
+ */
+
+export { ConversationLayout, default as ConversationLayoutDefault } from './ConversationLayout';
+export { ConversationHeader, default as ConversationHeaderDefault } from './ConversationHeader';
+export { MessageList, default as MessageListDefault } from './MessageList';
+export { MessageBubble, default as MessageBubbleDefault } from './MessageBubble';
+export { MessageActions, default as MessageActionsDefault } from './MessageActions';
+export { ChatInput, default as ChatInputDefault } from './ChatInput';
+export { StartScreen, default as StartScreenDefault } from './StartScreen';
+export { TypingIndicator, default as TypingIndicatorDefault } from './TypingIndicator';
+export { LoadingIndicator, default as LoadingIndicatorDefault } from './LoadingIndicator';
